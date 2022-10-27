d4a657e03da495acdf406988625d9f85157795ca
M Usman <ehmusman@gmail.com>
2022-10-27 12:10:01 +0500

    noyz is added

--- components/Projects.js
+++ components/Projects.js
@@ -18,6 +18,11 @@ const Projects = () => {
             title: "Swiss Block",
             link: "https://www.youtube.com/watch?v=X5jtFhoGQHI",
             description: "SWISS Block is a centralized exchange it is a similar exchange like FTX but some add on functionality like of paper trading as the main thing of this exchange is that it is totally gasless, it has all types of trades like Future, Spot, Margin trading, etc.I worked as a full-stack developer. It is designed using Reactjs, Redux Toolkit, Typescript, Nodejs, Expressjs, PostgreSQL, and Sequilize ORM"
+        },
+        {
+        title: "Noyz",
+        link: "https://www.youtube.com/watch?v=uq7dKYAN3b8",
+            description: "Noyz is a music-based NFT marketplace similar to Audius. Noyz is built on the Avalanche blockchain.The marketplace contains the following functionality e.g., Buying, NFT Listing on Market Page, Selling, Bidding, NFT , Royalty Fee Standards, Market Place commission Fee, User Proﬁles where users can listen to NFTS that they hold, Music Player, Users can follow the artist they like, Music Feed Section. In this project, I worked as a full- stack developer.The technologies that are used are listed here. Reactjs, Reduxjs, Nodejs, Expressjs, MongoDB, Metamask Wallet Connection, Etherjs, S3 bucket implementation for storing static Assets."
         }
     ]
   return (
